0ba1d3cfdd905a0995ebaf771b07d9080b6a43e5
apparettedare <ksuzuki9541@gmail.com>
2026-03-12 19:45:36 +0900

    update

--- scripts/atspace_score_manager.py
+++ scripts/atspace_score_manager.py
@@ -5,15 +5,18 @@ import rospy
 import smach
 import smach_ros
 import tf2_ros
-# from smach_files.goal_checker import GoalChecker
+from smach_files.goal_checker import GoalChecker
 
 class InitialState(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['success', 'fail'])
+        smach.State.__init__(self, outcomes=['success', 'fail'], output_keys=['initial_score'])
+        self.score     = rospy.get_param('~competition/initial_score', 0)
+        self.competition_time_limit = rospy.get_param('~competition/time_limit', 300)
     
     def execute(self, userdata):
         rospy.loginfo('=== Initial State ===')
         try:
+            userdata.initial_score = self.score
             return 'success'
         except Exception as e:
             rospy.logerr('System initialization error: %s', str(e))
@@ -22,13 +25,12 @@ class InitialState(smach.State):
 
 class StartTaskState(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['success', 'timeout', 'fail'])
+        smach.State.__init__(self, outcomes=['success', 'timeout', 'fail'], input_keys=['start_task_score'], output_keys=['start_task_score'])
     
     def execute(self, userdata):
         rospy.loginfo('=== Start Task State ===')
         try:
-            # if rospy.get_time() > 300:
-            #     return 'timeout'
+            userdata.start_task_score += 10
             return 'success'
         except Exception as e:
             rospy.logerr('Start task error: %s', str(e))
@@ -36,13 +38,11 @@ class StartTaskState(smach.State):
 
 class NavigationTaskState(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['success', 'timeout', 'fail'])
+        smach.State.__init__(self, outcomes=['success', 'timeout', 'fail'], input_keys=['navigation_task_score'], output_keys=['navigation_task_score'])
     
     def execute(self, userdata):
         rospy.loginfo('=== Navigation Task State ===')
         try:
-            # if rospy.get_time() > 300:
-            #     return 'timeout'
             return 'success'
         except Exception as e:
             rospy.logerr('Navigation task error: %s', str(e))
@@ -50,13 +50,11 @@ class NavigationTaskState(smach.State):
 
 class SearchTaskState(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['success', 'timeout', 'fail'])
-    
+        smach.State.__init__(self, outcomes=['success', 'timeout', 'fail'], input_keys=['search_task_score'], output_keys=['search_task_score'])
+
     def execute(self, userdata):
         rospy.loginfo('=== Search Task State ===')
         try:
-            # if rospy.get_time() > 300:
-            #     return 'timeout'
             return 'success'
         except Exception as e:
             rospy.logerr('Search task error: %s', str(e))
@@ -64,13 +62,11 @@ class SearchTaskState(smach.State):
 
 class DockingTaskState(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['success', 'timeout', 'fail'])
-    
+        smach.State.__init__(self, outcomes=['success', 'timeout', 'fail'], input_keys=['docking_task_score'], output_keys=['docking_task_score'])
+
     def execute(self, userdata):
         rospy.loginfo('=== Docking Task State ===')
         try:
-            # if rospy.get_time() > 300:
-            #     return 'timeout'
             return 'success'
         except Exception as e:
             rospy.logerr('Docking task error: %s', str(e))
@@ -78,8 +74,7 @@ class DockingTaskState(smach.State):
 
 class FinishState(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['success', 'fail'])
-    
+        smach.State.__init__(self, outcomes=['success', 'fail'], input_keys=['finish_score'])
     def execute(self, userdata):
         rospy.loginfo('=== Finish State ===')
         try:
@@ -89,39 +84,43 @@ class FinishState(smach.State):
             return 'fail'
 
 def main():    
-    # goal_checker = GoalChecker()
-    # object_detect_checker = ObjectDetectChecker()
     
     sm = smach.StateMachine(outcomes=['SUCCESS', 'FAIL'])
     
     with sm:
         smach.StateMachine.add('INITIAL', InitialState(),
                                transitions={'success': 'STARTTASK',
-                                           'fail': 'FAIL'})
+                                           'fail': 'FAIL'},
+                               remapping={'initial_score': 'score'})
         
         smach.StateMachine.add('STARTTASK', StartTaskState(),
                                transitions={'success': 'NAVIGATIONTASK',
                                             'timeout': 'FINISH',
-                                           'fail': 'FAIL'})
+                                           'fail': 'FAIL'},
+                               remapping={'start_task_score': 'score'})
 
         smach.StateMachine.add('NAVIGATIONTASK', NavigationTaskState(),
                                transitions={'success': 'SEARCHTASK',
                                             'timeout': 'FINISH',
-                                           'fail': 'FAIL'})
+                                           'fail': 'FAIL'},
+                               remapping={'navigation_task_score': 'score'})
 
         smach.StateMachine.add('SEARCHTASK', SearchTaskState(),
                                transitions={'success': 'DOCKINGTASK',
                                             'timeout': 'FINISH',
-                                           'fail': 'FAIL'})
+                                           'fail': 'FAIL'},
+                               remapping={'search_task_score': 'score'})
 
         smach.StateMachine.add('DOCKINGTASK', DockingTaskState(),
                                transitions={'success': 'FINISH',
                                             'timeout': 'FINISH',
-                                           'fail': 'FAIL'})
+                                           'fail': 'FAIL'},
+                               remapping={'docking_task_score': 'score'})
         
         smach.StateMachine.add('FINISH', FinishState(),
                                transitions={'success': 'SUCCESS',
-                                           'fail': 'FAIL'})
+                                           'fail': 'FAIL'},
+                               remapping={'finish_score': 'score'})
     
     sis = smach_ros.IntrospectionServer('robocup_atspace_score_manager_smach', sm, '/robocup_atspace_score_manager')
     sis.start()
